c08e3bd2535e55f0f80639e172a744388c51bd88
Vinay Gupta <VGUPTA4@volvocars.com>
2021-11-25 14:02:52 +0100

    eliminated not used

--- src/app/view/board/board.module.ts
+++ src/app/view/board/board.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { BoardRoutingModule } from './board-routing.module';
 import { TasksComponent } from './kanban/tasks/tasks.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { SectionComponent } from './kanban/tasks/sections/section/section.component';
-import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
