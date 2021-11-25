2fa5a8df24392ea7cc8ce0eddaf3a91afe757cbb
Vinay Gupta <VGUPTA4@volvocars.com>
2021-11-25 14:47:16 +0100

    empty array issue

--- src/app/common/util.ts
+++ src/app/common/util.ts
@@ -1,6 +1,11 @@
 import { TaskInfo } from "../view/board/kanban/model";
 
 export function maxID(tasks: TaskInfo[]): number {
-  const newtTask = tasks.reduce((p, c) => Number(p.id) > Number(c.id) ? p : c);
-  return + newtTask.id + 1;
+
+  if (tasks.length > 0) {
+    const newtTask = tasks.reduce((p, c) => Number(p.id) > Number(c.id) ? p : c);
+    return + newtTask.id + 1;
+  }
+
+  return 1;
 }
